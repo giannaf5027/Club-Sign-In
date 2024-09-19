ad90f8bf69a0d7f645ffe031e782b602fd61cc06
giannaf5027 <giannaef27@icloud.com>
2024-09-19 17:45:36 -0400

    added things

---
+++ scripts.js
@@ -0,0 +1,36 @@
+// poker logic
+
+
+//blackjack logic
+
+
+//codes
+
+function codes(){
+    getelementid()
+    if (input == "StopTheShow!"){
+        coin += 200;
+        code == null;
+    }
+}
+
+addEventListener();{
+    if(null){
+        print();
+    }
+}
+
+function welcome() {
+    setScreen("home");
+    console.log (("Hello " + nombre) + "! Pick lost or added to see the data for each.");
+  }
+  
+  function logout() {
+    setScreen("home");
+    hideElement("addb");
+    hideElement("lostb");
+    hideElement("datalbl");
+    showElement("insertname");
+    showElement("login");
+  }
+
